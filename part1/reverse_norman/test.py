d06e6dec3f9e1d8451365ac96765930767ee74ea
Michael Herman <hermanmu@gmail.com>
2014-12-26 13:42:04 -0600

    changed to xrange

--- part1/reverse_norman/test.py
+++ part1/reverse_norman/test.py
@@ -1,7 +1,7 @@
 from reverse_string import reverse
 
-print reverse('Hello World') == "dlroW olleH"         #True
-print reverse('eh') == 'he'                           #True
-print reverse('this is a TEST') == "TSET a si siht"   #True
-print reverse('football') == 'labtoof'                #False
-print reverse('False') == 'eslaf'                     #False
+print reverse('Hello World') == "dlroW olleH"         # True
+print reverse('eh') == 'he'                           # True
+print reverse('this is a TEST') == "TSET a si siht"   # True
+print reverse('football') == 'labtoof'                # False
+print reverse('False') == 'eslaf'                     # False
