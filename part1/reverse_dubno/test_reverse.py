1272eddb5886be5c5c096f37a99921e18a6b5576
Oleh Dubno <olehdubno@gmail.com>
2015-03-30 14:33:19 -0400

    changed the name of the folder

---
+++ part1/reverse_dubno/test_reverse.py
@@ -0,0 +1,16 @@
+from reverse_function import reverse 
+
+print reverse('New York') == 'kroY weN'
+print reverse('bike') == 'ekib'
+print reverse('new') == 'wen'
+print reverse('work') == 'krow'
+print reverse('skate') == 'etak'
+
+
+"""import unittest
+
+class TestReverse(unittest.TestCase):
+
+  def test_reverse(self):
+      self.assertEqual(reverse('hi', 'ih')
+"""
