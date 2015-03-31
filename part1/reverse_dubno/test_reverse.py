894969b8b09625b846d9e53495149ec92e84d080
Oleh Dubno <olehdubno@gmail.com>
2015-03-31 12:16:56 -0400

    added the unit tests for the reverse function

--- part1/reverse_dubno/test_reverse.py
+++ part1/reverse_dubno/test_reverse.py
@@ -5,12 +5,3 @@ print reverse('bike') == 'ekib'
 print reverse('new') == 'wen'
 print reverse('work') == 'krow'
 print reverse('skate') == 'etak'
-
-
-"""import unittest
-
-class TestReverse(unittest.TestCase):
-
-  def test_reverse(self):
-      self.assertEqual(reverse('hi', 'ih')
-"""
