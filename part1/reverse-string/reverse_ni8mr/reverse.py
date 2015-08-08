31623277febf65c4db32b7965e9a6e10627c9b48
Michael Herman <hermanmu@gmail.com>
2015-08-08 08:16:31 -0600

    minor updates

--- part1/reverse-string/reverse_ni8mr/reverse.py
+++
@@ -1,6 +0,0 @@
-import re
-
-my_string = raw_input("Enter your string: ")
-a = re.split('; |, |\*|\n', my_string)
-
-for i in 
--- part1/reverse-string/reverse_reza/test_reverse.py
+++ part1/reverse-string/reverse_reza/test_reverse.py
@@ -2,7 +2,6 @@ from reverse import reverse
 import unittest
 
 
-
 class ReverseTestCase(unittest.TestCase):
     def test(self):
         test_string = "Hello, My name is Reza. Github user name:ni8mr"
@@ -10,5 +9,5 @@ class ReverseTestCase(unittest.TestCase):
         result_string = reverse(test_string)
         self.assertEqual(altered_string, result_string)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
