9b7bf83e3c68a32d01ee8c0778c85ac9973063d0
ni8mr <nilpaddo@gmail.com>
2015-08-08 10:06:54 +0600

    Removing a direvtory created for testing

--- part2/reverse_flask_reza1/project/config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-
-WTF_CSRF_ENABLED = True
-SECRET_KEY = "secret"
--- part2/reverse_flask_reza1/project/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired, Length
-
-
-class FormToReverse(Form):
-    reverse = StringField('Reverse', validators=[DataRequired(), Length(min=2)])
