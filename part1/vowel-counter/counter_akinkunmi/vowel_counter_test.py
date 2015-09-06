d892b327dba596a9e0948b777118218452b3d5e0
nubianMONK <[gnosisdynamics@gmail.com]>
2015-09-06 00:31:20 -0400

    Test for vowel_counter executed

---
+++ part1/vowel-counter/counter_akinkunmi/vowel_counter_test.py
@@ -0,0 +1,9 @@
+from vowel_counter import vowel_counter
+
+def vowel_counter_test():
+	str = raw_input("Please input a string:  ")
+	vowel_counter(str)
+    
+	
+if __name__ == '__main__':
+    vowel_counter_test()	
